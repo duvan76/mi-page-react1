539a94912b1fbe213f0f61c7eb91ab7b32ab2ed8
duvan76 <116815556+duvan76@users.noreply.github.com>
2025-06-05 18:02:47 -0500

    Add files via upload

---
+++ database/restaurantes_db.sql
@@ -0,0 +1,9 @@
+ CREATE TABLE restaurantes ( 
+ id int NOT NULL AUTO_INCREMENT PRIMARY KEY,  
+ nombre varchar(255) DEFAULT NULL, 
+ direccion varchar(255) DEFAULT NULL,
+ descripcion text,  
+ imagen varchar(255) DEFAULT NULL
+ ) ;
+
+
